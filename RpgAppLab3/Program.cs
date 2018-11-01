d9df2d732bc57522be6cc3e58fcd0888fd4f0796
Johan Nensen <jnensen@hotmail.com>
2018-11-01 11:36:37 +0100

    cleaned up action branch

--- RpgAppLab3/Program.cs
+++ RpgAppLab3/Program.cs
@@ -18,20 +18,7 @@ namespace RpgAppLab3
                 var diceRoll = rnd.Next(20) + 1;
                 Console.WriteLine($"your dice roll was: {diceRoll}");
             }
-            if (command == "names")
-            {
 
-                var allNames = new[] { "Briana", "Ben", "David", "Dana", "Felicia", "Felix", "legolas", "Elrond"};
-
-                var randomIndex = rnd.Next(allNames.Length);
-                Console.WriteLine(allNames[randomIndex]);
-            }
-            if (command == "Tolkien names")
-            {
-                var tolkienNames = new[] { "legolas", "Elrond", "Frodo", "Aragaron", "Gandalf" };
-                var randomIndex = rnd.Next(tolkienNames.Length);
-                Console.WriteLine(tolkienNames[randomIndex]);
-            }
             if (command == "80s action")
             {
                 var actionNames = new[] { "Terminator", "Alien", "Leathal weapon" };
