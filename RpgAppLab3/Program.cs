55b169b308f2fddf1ab92afcad1903192fbc2730
Yen Agetorp <yen.agetorp@gmail.com>
2018-11-02 10:02:01 +0100

    Yen tog bort two last names

--- RpgAppLab3/Program.cs
+++ RpgAppLab3/Program.cs
@@ -21,7 +21,7 @@ namespace RpgAppLab3
             if (command == "names")
             {
 
-                var allNames = new[] { "Briana", "Ben", "David", "Dana", "Felicia", "Felix", "legolas", "Elrond"};
+                var allNames = new[] { "Briana", "Ben", "David", "Dana", "Felicia", "Felix"};
 
                 var randomIndex = rnd.Next(allNames.Length);
                 Console.WriteLine(allNames[randomIndex]);
